8e70dad4821db9e4559a9c79ab505456efd81d13
qadis1dev <alqadis1devops@gmail.com>
2024-01-18 20:41:45 +0400

    project init 2

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,10 @@ import {
   RouterProvider,
   createBrowserRouter
 } from "react-router-dom";
-import Slash from "./pages/Slash";
 
 const App:React.FC = () => {
   const router = createBrowserRouter(
-    [
-      { path: "/", element: <Slash /> }
-    ]
+    []
   );
 
   return <RouterProvider router={router} />
