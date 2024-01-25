35e0f79788eeb1331572657e77530dc2898693ed
qadis1dev <alqadis1devops@gmail.com>
2024-01-25 09:52:23 +0400

    added toaster to the app

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,23 @@ import {
   createBrowserRouter
 } from "react-router-dom";
 import Slash from "./pages/Slash";
+import Login from "./pages/Login";
+import { Toaster } from "@/components/ui/toaster";
 
 const App:React.FC = () => {
   const router = createBrowserRouter(
     [
-      { path: "/", element: <Slash /> }
+      { path: "/", element: <Slash /> },
+      { path: "/login", element: <Login /> }
     ]
   );
 
-  return <RouterProvider router={router} />
+  return(
+    <>
+      <Toaster />
+      <RouterProvider router={router} />
+    </>
+  );
 };
 
 export default App;
